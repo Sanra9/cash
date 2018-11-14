a2f4c09fc79deb65ecc4b5811c713a98abee3133
Santiago Ramírez <santiagoramirez@Student2s-MacBook-Pro.local>
2018-11-14 09:52:47 -0500

    Create route edit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -6,4 +6,30 @@ class CurrenciesController < ApplicationController
     def new
         @currency = Currency.new
     end
+
+    def create
+        @currency = Currency.new(currency_params) 
+        if @currency.save
+            redirect_to currencies_path, notice: 'Currency added'
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @currency = Currency.find(params[:id])
+    end
+
+    def show
+        @currency = Currency.find(params[:id])
+    end
+
+    def edit
+        @currency = Currency.find(params[:id])
+    end
+
+    private
+    def currency_params
+        params.require(:currency).permit(:name, :price_buy, :price_sale, :available)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/currencies', to: 'currencies#index'
-  get '/currencies/new', to: 'currencies#new'
+  get '/currencies/new', to: 'currencies#new', as: 'new_currency'
+  post 'currencies', to: 'currencies#create'
+  get '/currencies/:id/edit', to: 'currencies#edit' 
+  get '/currencies/:id', to: 'currencies#show'
 end
